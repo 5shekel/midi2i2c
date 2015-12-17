900c86a884a249ea8f5cda9bfb2d91dfa03e04af
yair reshef <yair99@gmail.com>
2015-12-17 16:25:14 +0200

    merged led and vixen and music

--- src/bivar/bivar.ino
+++ src/bivar/bivar.ino
@@ -1,6 +1,9 @@
 //neopixel lib starndtest + audio board tester for teensy
 //uncomment all for audio
-/*
+
+///////////////////////////////
+// using the audio module
+// http://www.pjrc.com/store/teensy3_audio.html
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -12,20 +15,46 @@ AudioOutputI2S           i2s1;
 AudioConnection          patchCord1(playSdWav1, 0, i2s1, 0);
 AudioConnection          patchCord2(playSdWav1, 1, i2s1, 1);
 AudioControlSGTL5000     sgtl5000_1;
-*/
+///////////////////////////////
+
+////////////////////////////
 #include <Adafruit_NeoPixel.h>
 #define PIN 2
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(32, PIN, NEO_GRB + NEO_KHZ800);
+////////////////////////////
 
-// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
-// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
-// and minimize distance between Arduino and first pixel.  Avoid connecting
-// on a live circuit...if you must, connect GND first.
+/////////////////////////////
+#define MAX_CHANNELS 15
+#include <PololuMaestro.h>
+#include <SoftwareSerial.h>
+SoftwareSerial srvSerial(3, 4); // RX, TX
+MiniMaestro maestro(srvSerial);
 
+#include <PrintEx.h>
+using namespace ios;
+///////////////////////////
 
+///////////////////////////
+int ch;
+int state;
+int chVal[MAX_CHANNELS] = {0};
+int prevChVal[MAX_CHANNELS] = {0};
+
+enum states
+{
+  IDLE,
+  DELIM,
+  READ,
+  PROCESS,
+  STORE
+};
+//////////////////////////
 
 void setup() {
-  /*
+  Serial.begin(57600);
+  srvSerial.begin(57600);
+
+  //AUDIO
   AudioMemory(8);
   sgtl5000_1.enable();
   sgtl5000_1.volume(0.5);
@@ -38,21 +67,98 @@ void setup() {
     }
   }
   delay(1000);
-*/
+
+  //LEDS
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+
+  //vixen
+  state = IDLE;
+  ch = 0;
 }
 
 void loop() {
+
+  if (Serial.available())
+  {
+    switch (state)
+    {
+      case IDLE:
+        ch = 0;
+        if (Serial.read() == '+')
+        {
+          state = DELIM;
+        }
+        else
+        {
+          state = IDLE;
+        }
+        break;
+
+      case DELIM:
+        ch = 0;
+        if (Serial.read() == '>')
+        {
+          state = READ;
+        }
+        else
+        {
+          state = IDLE;
+        }
+        break;
+
+      case READ:
+        chVal[ch++] = Serial.read();
+        if (ch >= MAX_CHANNELS)
+        {
+          ch = 0;
+          state = PROCESS;
+        }
+        break;
+
+      case PROCESS:
+        state = IDLE;
+
+        if (compareArrays(prevChVal, chVal, sizeof(prevChVal))) {
+          srvSerial << "same" << endl;
+          break; //only continue if the array is diffrent;
+
+        }
+        memcpy(prevChVal, chVal, sizeof(prevChVal));
+
+        for (int iii = 0; iii < MAX_CHANNELS; iii++)
+          srvSerial << iii << ":" << chVal[iii] << " | "; //print debug out
+
+        srvSerial << endl;
+        break;
+
+      case STORE:
+
+        break;
+    }
+  }
+  
   /*
-  if (playSdWav1.isPlaying() == false) {
+    if (playSdWav1.isPlaying() == false) {
     playSdWav1.play("sosimple.wav");
     delay(10); // wait for library to parse WAV info
-  }
+    }
+
+    rainbow(20);
+  
+  maestro.setTargetMiniSSC(servo[1], 255);
   */
-  rainbow(20);
 }
 
+
+
+int compareArrays(int a[], int b[], int n) {
+  for (int ii = 0; ii < n; ii++) {
+    if (a[ii] != b[ii]) return 0;
+    srvSerial << "not smae" << endl;
+  }
+  return 1;
+}
 
 void rainbow(uint8_t wait) {
   uint16_t i, j;
@@ -66,23 +172,9 @@ void rainbow(uint8_t wait) {
   }
 }
 
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
-    for (i = 0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
+  // Input a value 0 to 255 to get a color value.
+  // The colours are a transition r - g - b - back to r.
   WheelPos = 255 - WheelPos;
   if (WheelPos < 85) {
     return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
