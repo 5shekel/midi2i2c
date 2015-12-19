ff66ea99b7def388ed5657961aaf273dcb088744
yair reshef <yair99@gmail.com>
2015-12-19 22:28:24 +0200

    added neopixel on datapin 20

--- src/bivar/bivar.ino
+++ src/bivar/bivar.ino
@@ -17,6 +17,15 @@ AudioConnection          patchCord2(playSdWav1, 1, i2s1, 1);
 AudioControlSGTL5000     sgtl5000_1;
 ///////////////////////////////
 
+#include <Adafruit_NeoPixel.h>
+#define PIN 20
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(32, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
 ///////////////////////////
 
 #define MAX_CHANNELS 15
@@ -55,12 +64,25 @@ void setup() {
     }
   }
   delay(1000);
+
+  //LED
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
   //vixen
   state = IDLE;
   ch = 0;
 }
 
 void loop() {
+  //AUDIO
+  if (playSdWav1.isPlaying() == false) {
+    playSdWav1.play("sosimple.wav");
+    delay(10); // wait for library to parse WAV info
+  }
+  //LED
+  //rainbow(20);
+
   if (Serial.available())
   {
     switch (state)
@@ -137,3 +159,32 @@ boolean array_cmp(int *a, int *b, int len_a, int len_b) {
   //ok, if we have not returned yet, they are equal :)
   return true;
 }
+
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
