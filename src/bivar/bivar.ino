2469cecfdafee8e850dfac657f5b9fca4347274b
yair reshef <yair99@gmail.com>
2015-12-27 18:23:40 +0200

    with delata vixen and the lot

--- src/bivar/bivar.ino
+++ src/bivar/bivar.ino
@@ -1,6 +1,14 @@
 //neopixel lib starndtest + audio board tester for teensy
 //uncomment all for audio
 
+/*
+  serial for vixen,- pins 3 4 (TX,RX)
+  serial1 for pololu servo controller- pins 0 1 (RX1,TX1)
+  50 WS2812B LEDs - pin 20
+  teensy audio adaptor - pins 6,9,11,13,15,18,19,22,23 (18,19 i2c)
+  sd card via [audio adaptor]- pins 7, 10 12, 14 (SPI)
+*/
+
 ///////////////////////////////
 // using the audio module
 // http://www.pjrc.com/store/teensy3_audio.html
@@ -22,8 +30,7 @@ AudioControlSGTL5000     sgtl5000_1;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(32, PIN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
-// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
-// and minimize distance between Arduino and first pixel.  Avoid connecting
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input// and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 
 ///////////////////////////
@@ -31,6 +38,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(32, PIN, NEO_GRB + NEO_KHZ800);
 #define MAX_CHANNELS 15
 int ch;
 int state;
+long prevMillis, delta;
 int chVal[MAX_CHANNELS] = {0};
 int prevChVal[MAX_CHANNELS] = {0};
 
@@ -75,13 +83,14 @@ void setup() {
 }
 
 void loop() {
+
   //AUDIO
   if (playSdWav1.isPlaying() == false) {
     playSdWav1.play("sosimple.wav");
     delay(10); // wait for library to parse WAV info
   }
   //LED
-  //rainbow(20);
+  rainbow(20);
 
   if (Serial.available())
   {
@@ -123,12 +132,20 @@ void loop() {
       case PROCESS:
         state = IDLE;
 
-        if (array_cmp(prevChVal, chVal, 5, 5)) {
+        if (array_cmp(prevChVal, chVal, MAX_CHANNELS, MAX_CHANNELS)) {
           //Serial1.println("same");
+          for ( int i = 0 ; i < MAX_CHANNELS ; ++i )
+            prevChVal[ i ] = chVal[ i ];
           break; //only continue if the array is diffrent;
         }
 
-        memcpy(prevChVal, chVal, sizeof(prevChVal));
+        //copy array
+        for ( int i = 0 ;  i < MAX_CHANNELS ; ++i )
+          prevChVal[ i ] = chVal[ i ];
+
+        delta = millis() - prevMillis;
+        Serial1.print(delta);
+        Serial1.print("=");
 
         for (int iii = 0; iii < MAX_CHANNELS; iii++) {
           Serial1.print(iii);
